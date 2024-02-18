ed814cce9951995e264dace3fc8363e3837c3d2c
npuKoJlucTbl4 <159910054+npuKoJlucTbl4@users.noreply.github.com>
2024-02-18 16:53:53 +0600

    Initial commit

---
+++ sisi++.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+
+def parsing():
+    url = "https://www.pepper.ru/"
+
+    try:
+
+        driver = webdriver.Edge()
+        driver.get(url)
+        page = driver.page_source
+        soup = BeautifulSoup(page, "html.parser")
+        name = soup.findAll('strong' ,class_='thread-title')
+        temp = soup.findAll('span', class_='vote-temp')
+        link = soup.findAll('a', class_='thread-link')
+        for i in range(len(name)):
+            print(name[i].text)
+            print(temp[i].text.strip())                 #modern problems require modern solutions
+            print(link[i].get('href') + "\n")
+
+    finally:
+        driver.quit()
+
+if __name__ == "__main__":
+    parsing()
